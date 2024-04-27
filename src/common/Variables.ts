6eca1408a3ac654f70b92a7da3bfc5a86107da34
Kevin <koverly@gmail.com>
2024-04-27 17:17:34 -0500

    make sure api url is correct

--- src/common/Variables.ts
+++ src/common/Variables.ts
@@ -1,5 +1,5 @@
 //use this file to store all our global variables or anything environment specific
 export const variables={
-    API_URL: "http://192.168.1.12:5187"
-    //API_URL:"https://pheonixfitness.azurewebsites.net"
+    //API_URL: "http://192.168.1.12:5187"
+    API_URL:"https://pheonixfitness.azurewebsites.net"
 }
